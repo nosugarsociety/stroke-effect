e580254a77eb5ac96524e02ca181c55361216d7d
Jusang You <everflavor@gmail.com>
2021-02-11 16:35:10 -0500

    added parts of animation (logo, message a, svg path)

--- src/js/app.js
+++ src/js/app.js
@@ -21,8 +21,18 @@
       },
       values: {
         messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+        messageA_opacity_out: [1, 0, { start: 0.3, end: 0.4 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+        messageA_translateY_out: [0, -20, { start: 0.3, end: 0.4 }],
+
         pencilLogo_width_in: [1000, 200, { start: 0.1, end: 0.4 }],
+        pencilLogo_width_out: [200, 50, { start: 0.4, end: 0.8 }],
+        pencilLogo_translateX_in: [-10, -20, { start: 0.2, end: 0.4 }],
+        pencilLogo_translateX_out: [-20, -50, { start: 0.4, end: 0.8 }],
+        pencilLogo_opacity_out: [1, 0, { start: 0.8, end: 0.9 }],
+
         path_dashoffset_in: [1401, 0, { start: 0.2, end: 0.4 }],
+        path_dashoffset_out: [0, -1401, { start: 0.6, end: 0.8 }],
       },
     },
   ];
@@ -49,6 +59,102 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
   };
 
+  const calcValue = (values, currentYOffset) => {
+    let rv;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
+    if (values.length === 3) {
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = (values[1] - value[0]) * scrollRatio;
+    }
+
+    return rv;
+  };
+
+  const playAnimation = () => {
+    const { obj } = sceneInfo[currentScene];
+    const { values } = sceneInfo[currentScene];
+    const currentYOffset = yOffset - prevScrollHeight;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
+    switch (currentScene) {
+      case 0:
+        if (scrollRatio < 0.25) {
+          obj.messageA.style.opacity = calcValue(
+            values.messageA_opacity_in,
+            currentYOffset
+          );
+          obj.messageA.style.transform = `translate3d(0, ${calcValue(
+            values.messageA_translateY_in,
+            currentYOffset
+          )}%, 0)`;
+        } else {
+          obj.messageA.style.opacity = calcValue(
+            values.messageA_opacity_out,
+            currentYOffset
+          );
+          obj.messageA.style.transform = `translate3d(0, ${calcValue(
+            values.messageA_translateY_out,
+            currentYOffset
+          )}%, 0)`;
+        }
+
+        if (scrollRatio <= 0.4) {
+          obj.pencilLogo.style.width = `${calcValue(
+            values.pencilLogo_width_in,
+            currentYOffset
+          )}vw`;
+
+          obj.pencilLogo.style.transform = `translate3d(${calcValue(
+            values.pencilLogo_translateX_in,
+            currentYOffset
+          )}%, -50%, 0)`;
+        } else {
+          obj.pencilLogo.style.width = `${calcValue(
+            values.pencilLogo_width_out,
+            currentYOffset
+          )}vw`;
+          obj.pencilLogo.style.transform = `translate3d(${calcValue(
+            values.pencilLogo_translateX_out,
+            currentYOffset
+          )}%, -50%, 0})`;
+        }
+
+        if (scrollRatio < 0.5) {
+          obj.ribbonPath.style.strokeDashoffset = calcValue(
+            values.path_dashoffset_in,
+            currentYOffset
+          );
+        } else {
+          obj.ribbonPath.style.strokeDashoffset = calcValues(
+            values.path_dashoffset_out,
+            currentYOffset
+          );
+        }
+
+        break;
+    }
+  };
+
   const scrollLoop = () => {
     enterNewScene = false;
     prevScrollHeight = 0;
@@ -56,10 +162,6 @@
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
-
-    // if (yOffset < prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
-    //   document.body.classList.remove('scroll-effect-end');
-    // }
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       enterNewScene = true;
@@ -77,7 +179,6 @@
     }
 
     if (yOffset < prevScrollHeight) {
-      console.log('yes');
       enterNewScene = true;
       if (currentScene === 0) return;
       currentScene--;
@@ -85,6 +186,7 @@
     }
 
     if (enterNewScene) return;
+    playAnimation();
   };
 
   window.addEventListener('load', () => {
