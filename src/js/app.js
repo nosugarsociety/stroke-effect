adcfa0b6d33b5b2cd05155171470fda398696e3d
Jusang You <everflavor@gmail.com>
2021-02-11 11:10:39 -0500

    setLayout function added.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
+/* eslint-disable */
 (() => {
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+  let enterNewScene = false;
+
   const sceneInfo = [
     {
       // Scene 0
@@ -18,10 +24,24 @@
 
   const setLayout = () => {
     for (let i = 0; i < sceneInfo.length; i++) {
-      // if (sceneInfo[i] === 'sticky') {
-      // sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      // }
+      if (sceneInfo[i].type === 'sticky') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      }
+      sceneInfo[
+        i
+      ].obj.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo[i].length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
   };
 
   window.addEventListener('load', () => {
